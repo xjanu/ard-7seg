9fde900dea5ffebb6b744819e9d5e49921be2692
Martin Janů <xjanu@fi.muni.cz>
2022-07-26 17:49:54 +0200

    Debugging output

--- 7seg.ino
+++ 7seg.ino
@@ -9,10 +9,28 @@ SegmentDisplay display{CATHODE, segment_pins, digit_pins[0],
 
 void setup() {
   display.set_freq(display_freq);
+  for (unsigned i = 0; i < 4; ++i) {
+    display.raw_write(i, 0b11111111u);
+    unsigned start = millis();
+    while (millis() - start < 1000) {
+      display.refresh();
+    }
+    display.raw_write(i, 0b0u);
+  }
+  for (unsigned char i = 0b10000000; i != 0x00; i >>= 1) {
+    display.raw_write(0, i);
+    display.raw_write(1, i);
+    display.raw_write(2, i);
+    display.raw_write(3, i);
+    unsigned start = millis();
+    while (millis() - start < 1000) {
+      display.refresh();
+    }
+  }
 }
 
 void loop() {
   unsigned now = millis();
-  display.write_num(now % 10000);
+  display.write_num( (now / 10) % 10000 );
   display.refresh();
 }
