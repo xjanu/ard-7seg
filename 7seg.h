f5a94c62fc2bed525068609f959a5b0b4e1ac527
Martin Janů <xjanu@fi.muni.cz>
2022-08-22 16:32:42 +0200

    Add documentation

--- 7seg.h
+++ 7seg.h
@@ -3,6 +3,21 @@
 #define CATHODE true
 #define ANODE   false
 
+/**
+ * @anchor segment_layout
+ * @par Segment Layout
+ * @verbatim
++---7---+
+|       |
+2       6
+|       |
++---1---+
+|       |
+3       5
+|       |
++---4---+  0
+   @endverbatim
+ */
 class SegmentDisplay
 {
   int seg_pins[8];
@@ -31,25 +46,60 @@ public:
     0b11110110u
   };
 
-  /*
-  +-7-+
- 2|   |6
-  +-1-+
- 3|   |5
-  +-4-+ 0
-  */
-
+  /**
+   * @brief Construct a new object representing a 7-segment display
+   *
+   * @param cathode  true if display is common cathode. Also possible to use
+   *                 CATHODE or ANODE macros
+   * @param segments Array of pin numbers used to address individual display
+   *                 segments, in descending order according to the
+   *                 @ref segment_layout "Segment Layout"
+   * @param digit0, digit1, digit2, digit3
+   *        pin number used to address Nth digit from the left,
+   *        i.e. digit0 is the least significant digit
+   */
   SegmentDisplay(bool cathode, const int segments[8],
                  int digit0, int digit1, int digit2, int digit3);
   SegmentDisplay(bool cathode, const int segments[8], int digit0, int digit1, int digit2);
   SegmentDisplay(bool cathode, const int segments[8], int digit0, int digit1);
   SegmentDisplay(bool cathode, const int segments[8], int digit0);
 
+  /**
+   * @brief Low-level write
+   *
+   * @param digit which digit to write to
+   * @param bits  which segments to light up. The
+   *              @ref segment_layout "Segment Layout" section maps between bits
+   *              and segments. 0 is the least-significant bit.
+   * @return true on success
+   */
   bool raw_write(int digit, unsigned char bits);
 
+  /**
+   * @brief Set the desired refresh frequency
+   *
+   * @note Note that this is in fact the theoretical maximum frequency the
+   *       display will operate at. The real frequency is dependent on how
+   *       often the @ref refresh method is called.
+   *
+   * @param frequency in Hz
+   * @return true on success
+   */
   bool set_freq(int frequency);
 
+  /**
+   * @brief Refresh the display
+   *
+   * This method should be called repeatedly in a loop as fast as possible, else
+   * the display will freeze, showing only one digit lit up.
+   */
   void refresh();
 
+  /**
+   * @brief Write a number to the display.
+   *
+   * @param n The number to be written
+   * @return true on success.
+   */
   bool write_num(int n);
 };
